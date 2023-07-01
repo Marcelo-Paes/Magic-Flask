7de262139604c0ad99722054eb1b517d25abebf1
Marcelo <marcelo19paes@gmail.com>
2023-07-01 18:07:09 -0300

    magic_bug_duplicate_corrigido

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import os
 os.environ['EXCHANGERATE_API_KEY'] = 'sua_chave_de_api'
 app = Flask(__name__)
 
-app.secret_key = 'sua_chave_secreta'  # Defina sua chave secreta aqui
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -23,6 +21,17 @@ def search():
     card_price_brl = convert_to_brl(card_price_usd)
     return render_template('result.html', card_name=card_name, card_image=card_image, card_description=card_description, card_price_brl=card_price_brl)
 
+
+@app.route('/result')
+def result():
+    card_name = request.args.get('card_name')
+    response = requests.get(f'https://api.scryfall.com/cards/named?fuzzy={card_name}')
+    card_data = json.loads(response.text)
+    card_image = card_data['image_uris']['normal']
+    card_description = card_data['oracle_text']
+    card_price = card_data['prices']['usd']
+    return render_template('result.html', card_name=card_name, card_image=card_image, card_description=card_description, card_price=card_price)
+
 def convert_to_brl(price_usd):
     response = requests.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json')
     exchange_data = json.loads(response.text)
@@ -35,13 +44,15 @@ def add_deck():
     if request.method == 'POST':
         deck_name = request.form['deck_name']
         deck_list = request.form['deck_list']
-        
-        file = request.files['file_input']
-        if file:
-            file_content = file.read().decode('utf-8')
-            deck_list += '\n' + file_content
-        
-        card_names = deck_list.split('\n')  # Assume que as cartas estão separadas por linhas
+
+        # Processar o arquivo de texto se fornecido
+        if 'file_input' in request.files:
+            file = request.files['file_input']
+            if file.filename.endswith('.txt'):
+                card_names = file.read().decode('utf-8').splitlines()
+                card_names = [card.strip() for card in card_names if card.strip()]
+
+        card_names = [card.strip() for card in card_names]  # Assume que as cartas estão separadas por linhas
         
         cards = []
         for card_name in card_names:
@@ -58,6 +69,7 @@ def add_deck():
         return render_template('deck.html', deck_name=deck_name, deck_list=cards, deck_formats=deck_formats)
     
     return render_template('add_deck.html')
+
 
 def get_deck_formats(deck_list):
     formats = ['Standard', 'Modern', 'Legacy', 'Vintage']  # Exemplo de formatos disponíveis
